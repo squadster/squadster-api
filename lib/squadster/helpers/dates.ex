64497f9ba299a2d2d2bcdfa07725107a253e8f30
Alexander Teslovskiy <artoriousso@gmail.com>
2020-01-31 21:00:18 +0300

    Refactor dates helper

--- lib/squadster/helpers/dates.ex
+++ lib/squadster/helpers/dates.ex
@@ -2,7 +2,7 @@ defmodule Squadster.Helpers.Dates do
   def date_from_string(string_date) do
     [day, month, year] = string_date \
     |> String.split(".")
-    |> Enum.map(fn element -> String.to_integer(element) end)
+    |> Enum.map(&String.to_integer/1)
 
     case Date.new(year, month, day) do
       {:ok, date} -> date
