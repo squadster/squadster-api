f0e3053ee75688d4a8621c3961286a579df25e92
Alexander Teslovskiy <artoriousso@gmail.com>
2020-02-02 18:57:22 +0300

    Add build stage

--- config/test.travis.exs
+++ config/test.travis.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :squadster, Squadster.Repo,
   username: "postgres",
   password: "",
-  database: "travis_ci_test",
+  database: (System.get_env("DB_BASE_NAME") || "squadster") <> "_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
