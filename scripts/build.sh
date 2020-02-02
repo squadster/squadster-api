6d41929f9b9d211c7bec0bb915d6a4723e716f67
Alexander Teslovskiy <artoriousso@gmail.com>
2020-02-02 20:46:55 +0300

    Add image building to .travis.yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@
 MIX_ENV=prod mix compile
 MIX_ENV=prod mix release
 
-docker build . --tag squadster
-
-# TODO: push to registry here
+echo $GITHUB_TOKEN | docker login -u $GITHUB_USER --password-stdin docker.pkg.github.com
+docker build -t docker.pkg.github.com/$GITHUB_ORGANIZATION/$GITHUB_REPO/$GITHUB_PACKAGE:$RELEASE_VERSION .
+docker push docker.pkg.github.com/$GITHUB_ORGANIZATION/$GITHUB_REPO/$GITHUB_PACKAGE:$RELEASE_VERSION
