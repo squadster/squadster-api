0106cfc3841577df09fa0ca933b156afd2a02784
Alexander Teslovskiy <artoriousso@gmail.com>
2020-02-19 16:01:56 +0300

    Refactor

--- priv/repo/seeds.dev.exs
+++ priv/repo/seeds.dev.exs
@@ -4,7 +4,9 @@ alias Squadster.Accounts.User
 alias Squadster.Formations.Squad
 alias Squadster.Formations.SquadMember
 
-for _ <- (1..30) do
+seeds_config = [users: 30, squads: 2, users_per_squad: 15]
+
+for _ <- (1..seeds_config[:users]) do
   uid = Faker.Util.format("%9d")
   Repo.insert!(%User{
     first_name: Faker.Name.first_name,
@@ -19,49 +21,26 @@ for _ <- (1..30) do
     image_url: Faker.Avatar.image_url,
     vk_url: "https://vk.com/id" <> uid
   })
-  #has_one :squad_member, Squadster.SquadMember
 end
 
-for _ <- (1..2) do
+for _ <- (1..seeds_config[:squads]) do
   Repo.insert!(%Squad{
     squad_number: Faker.Util.format("%6d"),
     advertisment: Faker.StarWars.quote,
     class_day: Faker.Util.pick([:monday, :twesday, :wednesday, :thursday, :friday, :saturday])
   })
-  #has_many :squad_members, Squadster.Formations.SquadMember
 end
 
-squad = Repo.get(Squad, 1)
+for index <- (1..seeds_config[:squads]) do
+  squad = Repo.get(Squad, index)
 
-Repo.all(User)
-|> Enum.slice(0, 15)
-|> Enum.each fn user ->
-  Repo.insert!(%SquadMember{
-    role: Faker.Util.pick([:commander, :deputy_commander, :journalist, :student]), # TODO one
-    user: user,
-    squad: squad
-  })
+  Repo.all(User)
+  |> Enum.slice((index - 1) * seeds_config[:users_per_squad], seeds_config[:users_per_squad])
+  |> Enum.each fn user ->
+    Repo.insert!(%SquadMember{
+      role: :student,
+      user: user,
+      squad: squad
+    })
+  end
 end
-
-squad = Repo.get(Squad, 2)
-
-Repo.all(User)
-|> Enum.slice(15, 15)
-|> Enum.each fn user ->
-  Repo.insert!(%SquadMember{
-    role: Faker.Util.pick([:commander, :deputy_commander, :journalist, :student]), # TODO one
-    user: user,
-    squad: squad
-  })
-end
-
-
-#Enum.each squads, fn squad ->
-  #Enum.each users, fn user ->
-    #Repo.insert!(%SquadMember{
-      #role: Faker.Util.pick([:commander, :deputy_commander, :journalist, :student]), # TODO one
-      #user: Repo.get(User, user.id * squad.id),
-      #squad: squad
-    #})
-  #end
-#end
