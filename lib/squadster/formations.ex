f35979de233ba4157e46cf54ab8335f153a0212a
mirterious <inudad@yandex.ru>
2020-02-23 15:38:49 +0300

    implement user validation on creating a squad

--- lib/squadster/formations.ex
+++ lib/squadster/formations.ex
@@ -1,6 +1,8 @@
 defmodule Squadster.Formations do
   alias Squadster.Repo
+  alias Squadster.Accounts.User
   alias Squadster.Formations.Squad
+  alias Squadster.Formations.SquadMember
 
   def data() do
     Dataloader.Ecto.new(Repo, query: &query/2)
@@ -18,10 +20,33 @@ defmodule Squadster.Formations do
     Squad |> Repo.get_by(squad_number: number)
   end
 
-  def create_squad(args) do
-    args
-    |> Squad.changeset
-    |> Repo.insert
+  def create_squad(args, user) do
+    user = user |> Repo.preload(:squad_member)
+    case user.squad_member do
+      nil ->
+        args
+        |> Squad.changeset
+        |> Repo.insert
+        |> create_squad_member(user)
+      _member -> {:error, "Delete existing squad to create new one"}
+    end
+  end
+
+  # TODO: Refactor member creation with changeset
+  defp create_squad_member(squad, user) do
+    case squad do
+      {:ok, squadster} ->
+        # new_member = %SquadMember{role: 0, user: user, squad: squadster}
+        # |> SquadMember.changeset
+        # |> Repo.insert
+        Repo.insert!(%SquadMember{
+          role: :commander,
+          user: user,
+          squad: squadster
+        })
+        {:ok, squadster}
+      {:error, reason} -> {:error, reason}
+    end
   end
 
   def update_squad(args) do
