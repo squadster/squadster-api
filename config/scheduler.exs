6195884ab1adcc638471983d45b0afff594bb295
Alexander Teslovskiy <artoriousso@gmail.com>
2020-03-02 01:32:44 +0300

    Update schedule

--- config/scheduler.exs
+++ config/scheduler.exs
@@ -6,14 +6,14 @@ config :squadster, Squadster.Workers,
   timeout: 30_000,
 
   jobs: [
-    # shift duty queues every midnight
+    # shift duty queues every day at 01:00 am
     shift_queue_numbers: [
-      schedule: "@daily",
+      schedule: "0 1 * * *",
       task: {Workers.ShiftQueueNumbers, :start, []}
     ],
-    # remind about duty every midnight
+    # remind about duty every midday
     notify_attendants: [
-      schedule: "@daily",
+      schedule: "0 12 * * *",
       task: {Workers.NotifyAttendants, :start, []}
     ]
   ]
