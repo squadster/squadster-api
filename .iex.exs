980f3e9e498821be73d692652dcef5df142d0b54
Alexander Teslovskiy <artoriousso@gmail.com>
2020-03-06 17:43:17 +0300

    Add .iex.exs, update deps

---
+++ .iex.exs
@@ -0,0 +1,12 @@
+import Ecto.Query
+
+alias Squadster.Repo
+
+alias Squadster.Accounts
+alias Squadster.Accounts.User
+
+alias Squadster.Formations
+alias Squadster.Formations.{Squad, SquadRequest, SquadMember}
+
+alias Squadster.Helpers
+alias Squadster.Helpers.{Dates, Permissions}
