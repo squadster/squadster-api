d2c4b08abe2cce72d9f87aa78f89525403e45973
Ilya Vasilevich <inudad@yandex.ru>
2020-03-13 18:41:28 +0300

    Add request sample

--- lib/squadster/workers/notify_attendants.ex
+++ lib/squadster/workers/notify_attendants.ex
@@ -1,6 +1,9 @@
 defmodule Squadster.Workers.NotifyAttendants do
   use Task
 
+  @bot_endpoint "https://mercury-dev.herokuapp.com/message"
+  # request_example: HTTPoison.post "https://mercury-dev.herokuapp.com/message", "{\"text\": \"FUCK YOU YOU FUCKING FUCK\", \"target\": 1488}", [{"content-type", "application/json"}]
+
   def start_link(args) do
     Task.start_link(__MODULE__, :run, [args])
   end
