eef34441f0bc0a5679626d9e5c4c6f45dc43ace9
mirterious <inudad@yandex.ru>
2020-03-13 23:48:08 +0300

    Implement notify worker

--- lib/squadster/workers/notify_attendants.ex
+++ lib/squadster/workers/notify_attendants.ex
@@ -1,14 +1,39 @@
 defmodule Squadster.Workers.NotifyAttendants do
   use Task
 
+  import Ecto.Query
+  import HTTPoison
+
+  alias Squadster.Formations.SquadMember
+  alias Squadster.Repo
+
   @bot_endpoint "https://mercury-dev.herokuapp.com/message"
-  # request_example: HTTPoison.post "https://mercury-dev.herokuapp.com/message", "{\"text\": \"FUCK YOU YOU FUCKING FUCK\", \"target\": 1488}", [{"content-type", "application/json"}]
+  @message "Завтра вы ответственный за лопату!"
+  @request_headers [{"content-type", "application/json"}]
 
   def start_link(args) do
     Task.start_link(__MODULE__, :run, [args])
   end
 
   def run(_args) do
-    # TODO: implement
+    next_day_duties = from(member in SquadMember, where: member.queue_number == 1)
+    |> Repo.all
+    |> Repo.preload(:user)
+    |> Enum.each fn duty ->
+      notify(duty.user)
+    end
+  end
+
+  defp notify(duty) do
+    HTTPoison.post @bot_endpoint, request_body(duty.id), @request_headers
+  end
+
+  def request_body(duty_id) do
+    """
+      {
+        "text": "#{@message}",
+        "targer": #{duty_id}
+      }
+    """
   end
 end
