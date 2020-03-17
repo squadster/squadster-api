099c6bec7ee35b106b6a6a52de4ea097b2eebc87
Alexander Teslovskiy <artoriousso@gmail.com>
2020-03-17 16:54:45 +0300

    Add test cases

--- spec/web/schema/squad_requests_spec.exs
+++ spec/web/schema/squad_requests_spec.exs
@@ -6,4 +6,35 @@ defmodule Squadster.Web.Schema.SquadRequestSpec do
   import Squadster.Support.Factory
 
   alias Squadster.Formations.SquadRequest
+
+  let :create do
+    """
+      mutation createSquadRequest($squad_id: Int) {
+        createSquadRequest(squad_id: $squad_id) {
+          squad
+        }
+      }
+    """
+  end
+
+  let :create_squad_request_query, do: %{query: create(), variables: %{squad_id: squad().id}}
+
+  let :squad, do: build(:squad)
+  let :user, do: insert(:user)
+
+  def api_request(payload) do
+    login_as(user()) |> query(payload)
+  end
+
+  def entities_count(struct) do
+    struct |> Repo.all |> Enum.count
+  end
+
+  describe "mutations" do
+    it "creates a new squad_request with valid attributes" do
+      previous_count = entities_count(SquadRequest)
+      api_request(create_squad_request_query())
+      expect entities_count(SquadRequest) |> to(eq previous_count + 1)
+    end
+  end
 end
