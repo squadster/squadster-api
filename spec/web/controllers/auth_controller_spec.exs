27a4b12216703192f1cb4f128526b4aaf37d294f
Alexander Teslovskiy <artoriousso@gmail.com>
2020-03-18 16:30:39 +0300

    Refactor

---
+++ spec/web/controllers/auth_controller_spec.exs
@@ -0,0 +1,12 @@
+defmodule Squadster.Web.AuthControllerSpec do
+  use ESpec.Phoenix, async: true
+  use ESpec.Phoenix.Extend, :controller
+
+  describe "GET /api/auth/callback" do
+
+  end
+
+  describe "DELETE /api/auth" do
+
+  end
+end
