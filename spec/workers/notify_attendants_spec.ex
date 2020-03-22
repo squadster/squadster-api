1592539c4d092d2e810e61db98faee5bd7f1af4e
Ilya Vasilevich <inudad@yandex.ru>
2020-03-22 14:51:34 +0300

    Add notify attendants worker spec sample

---
+++ spec/workers/notify_attendants_spec.ex
@@ -0,0 +1,8 @@
+# TODO: Check, if worker send correct data to bot
+defmodule Squadster.Workers.NotifyAttendantsSpec do
+  use ESpec
+
+  it "Sends request to bot with valid params" do
+    # expect HTTPoison |> to(receivce :valid_params)
+  end
+end
