3d9e71fa038b6ea292cf42a0dcbd78ed401db8f3
Alexander Teslovskiy <artoriousso@gmail.com>
2020-04-02 19:54:56 +0300

    Refactor code duplication in permissions

--- lib/squadster/helpers/permissions.ex
+++ lib/squadster/helpers/permissions.ex
@@ -3,55 +3,54 @@ defmodule Squadster.Helpers.Permissions do
   alias Squadster.Accounts.User
   alias Squadster.Formations.{Squad, SquadRequest, SquadMember}
 
-  def can_update?(%User{} = user, %Squad{} = squad) do
-    %{squad_member: member} = user |> Repo.preload(squad_member: :squad)
+  @management_roles [:commander, :deputy_commander, :journalist]
 
-    cond do
-      is_nil(member) -> false
-      member.role in [:commander, :deputy_commander, :journalist] and squad == member.squad -> true
-      true -> false
-    end
+  def can_update?(%User{} = user, %Squad{} = squad) do
+    user |> has_management_role_in?(squad)
   end
 
   def can_update?(%User{} = user, %SquadRequest{} = squad_request) do
-    %{squad_member: approver} = user |> Repo.preload(squad_member: :squad)
     %{squad: squad} = squad_request |> Repo.preload(:squad)
-
-    cond do
-      is_nil(approver) -> false
-      approver.role in [:commander, :deputy_commander, :journalist] and squad == approver.squad -> true
-      true -> false
-    end
+    user |> has_management_role_in?(squad)
   end
 
   def can_update?(%User{} = user, %SquadMember{} = squad_member) do
     %{squad: squad, user: member_user} = squad_member |> Repo.preload(:squad) |> Repo.preload(:user)
-    member_user != user && can_delete?(user, squad)
+    member_user != user && (user |> can_delete?(squad))
   end
 
   def can_delete?(%User{} = user, %Squad{} = squad) do
-    %{squad_member: member} = user |> Repo.preload(squad_member: :squad)
-
-    cond do
-      is_nil(member) -> false
-      member.role in [:commander] and squad == member.squad -> true
-      true -> false
-    end
+    user |> has_commander_role_in?(squad)
   end
 
   def can_delete?(%User{} = user, %SquadRequest{} = squad_request) do
-    %{squad_member: member} = user |> Repo.preload(squad_member: :squad)
     %{squad: squad, user: request_user} = squad_request |> Repo.preload(:squad) |> Repo.preload(:user)
+    request_user == user || (user |> has_management_role_in?(squad))
+  end
+
+  def can_delete?(%User{} = user, %SquadMember{} = squad_member) do
+    user |> can_update?(squad_member)
+  end
+
+  defp has_commander_role_in?(%User{} = user, %Squad{} = squad) do
+    user |> has_role_in?(squad, [:commander])
+  end
+
+  defp has_management_role_in?(%User{} = user, %Squad{} = squad) do
+    user |> has_role_in?(squad, @management_roles)
+  end
+
+  defp has_role_in?(%User{} = user, %Squad{} = squad, roles) when is_list(roles) do
+    %{squad_member: member} = user |> Repo.preload(squad_member: :squad)
 
     cond do
-      request_user == user -> true
       is_nil(member) -> false
-      member.role in [:commander, :deputy_commander, :journalist] and squad == member.squad -> true
+      member.role in roles and squad == member.squad -> true
       true -> false
     end
   end
 
-  def can_delete?(%User{} = user, %SquadMember{} = squad_member) do
-    can_update?(user, squad_member)
+  defp has_role_in?(%User{} = user, %Squad{} = squad, role) do
+    user |> has_role_in?(squad, [role])
   end
 end
