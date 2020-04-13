4603ec0c1ee944b6f01de8f7a8fa2b93a801e703
Alexander Teslovskiy <artoriousso@gmail.com>
2020-04-13 21:16:45 +0300

    Refactor worker, add default value

--- lib/squadster/workers/shift_queue_numbers.ex
+++ lib/squadster/workers/shift_queue_numbers.ex
@@ -27,20 +27,21 @@ defmodule Squadster.Workers.ShiftQueueNumbers do
 
   defp update_query(members) do
     members =
-      members |> Enum.filter(fn %{queue_number: queue_number} -> !is_nil(queue_number) end)
+      members
+      |> Enum.reject(fn %{queue_number: queue_number} -> is_nil(queue_number) end)
 
-    %{queue_number: last_number} = Enum.max_by(members, &(&1.queue_number))
+    %{queue_number: last_number} = Enum.max_by(members, &(&1.queue_number), fn -> %{queue_number: 1} end)
 
-    members |> Enum.each(fn member -> update_member(member, last_number) end)
+    members |> Enum.each(fn member -> member |> update(last_number) end)
   end
 
-  defp update_member(%SquadMember{queue_number: 1} = member, last_number) do
+  defp update(%SquadMember{queue_number: 1} = member, last_number) do
     member
     |> SquadMember.changeset(%{queue_number: last_number})
     |> Repo.update
   end
 
-  defp update_member(member, _last_number) do
+  defp update(member, _last_number) do
     member
     |> SquadMember.changeset(%{queue_number: member.queue_number - 1})
     |> Repo.update
