57b44f43bd836dfa1a3235d242d1e3de4f52feeb
Alexander Teslovskiy <artoriousso@gmail.com>
2020-04-18 19:51:14 +0300

    Add factories to seeds

--- priv/repo/seeds.dev.exs
+++ priv/repo/seeds.dev.exs
@@ -1,7 +1,7 @@
 import Ecto.Query
+import Squadster.Support.Factory
 
 alias Squadster.Repo
-
 alias Squadster.Accounts.User
 alias Squadster.Formations.Squad
 alias Squadster.Formations.SquadMember
@@ -13,31 +13,10 @@ seeds_config = [
 ]
 
 # create users
-for _ <- (1..seeds_config[:users]) do
-  uid = Faker.Util.format("%9d")
-  Repo.insert!(%User{
-    first_name: Faker.Name.first_name,
-    last_name: Faker.Name.last_name,
-    birth_date: Faker.Date.date_of_birth(17..20),
-    mobile_phone: "+375" <> Faker.Util.format("%9d"),
-    university: "University of " <> Faker.Industry.sector,
-    faculty: "Faculty of " <> Faker.Industry.sub_sector,
-    uid: uid,
-    auth_token: Faker.String.base64(85),
-    small_image_url: Faker.Avatar.image_url,
-    image_url: Faker.Avatar.image_url,
-    vk_url: "https://vk.com/id" <> uid
-  })
-end
+for _ <- (1..seeds_config[:users]), do: insert(:user)
 
 # create squads
-for _ <- (1..seeds_config[:squads]) do
-  Repo.insert!(%Squad{
-    squad_number: Faker.Util.format("%6d"),
-    advertisment: Faker.StarWars.quote,
-    class_day: Faker.Util.pick([:monday, :tuesday, :wednesday, :thursday, :friday, :saturday])
-  })
-end
+for _ <- (1..seeds_config[:squads]), do: insert(:squad)
 
 # connect users and squads
 for index <- (1..seeds_config[:squads]) do
@@ -45,13 +24,7 @@ for index <- (1..seeds_config[:squads]) do
 
   Repo.all(User)
   |> Enum.slice((index - 1) * seeds_config[:users_per_squad], seeds_config[:users_per_squad])
-  |> Enum.each(fn user ->
-    Repo.insert!(%SquadMember{
-      role: :student,
-      user: user,
-      squad: squad
-    })
-  end)
+  |> Enum.each(fn user -> insert(:squad_member, user: user, squad: squad, role: :student) end)
 end
 
 # mark first 3 members as main roles
