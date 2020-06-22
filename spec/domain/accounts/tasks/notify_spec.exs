c431c1c10c00c640e2c2f0f0a93e9a9b95a748d8
Alexander Teslovskiy <artoriousso@gmail.com>
2020-06-23 02:30:32 +0300

    Fix unique PK constraint for parallel tests

--- spec/domain/accounts/tasks/notify_spec.exs
+++ spec/domain/accounts/tasks/notify_spec.exs
@@ -1,3 +1,11 @@
+# This test using hard-coded matches with explisit ids,
+# each spec has its own let(:user) with unique id,
+# this is done to not violate PK constraint in parallel
+# testing, but this is not the best solution.
+# TODO:
+# Mockery allows dynamic matches with macros, so this
+# test should be reworked to use macros for id matches.
+#
 defmodule Squadster.Accounts.Tasks.NotifySpec do
   use ESpec.Phoenix, async: true
   use ESpec.Phoenix.Extend, :domain
@@ -11,13 +19,14 @@ defmodule Squadster.Accounts.Tasks.NotifySpec do
 
   describe "notify/1" do
     let :message, do: "Who is John Galt?"
-    let :user, do: insert(:user, id: 1)
 
     before do
       mock HTTPoison, :post
     end
 
     context "when the target is user" do
+      let :user, do: insert(:user, id: 1)
+
       it "sends message to this user" do
         Notify.notify(message: message(), target: user())
         assert_called HTTPoison, :post, [
@@ -34,6 +43,7 @@ defmodule Squadster.Accounts.Tasks.NotifySpec do
     end
 
     context "when the target is a squad" do
+      let :user, do: insert(:user)
       let :squad, do: build(:squad) |> with_commander(user()) |> insert
 
       before do
@@ -48,6 +58,8 @@ defmodule Squadster.Accounts.Tasks.NotifySpec do
 
       describe "options" do
         context "when skip_commander set to true" do
+          let :user, do: insert(:user, id: 2)
+
           it "does not send message to squad commander" do
             Notify.notify(message: message(), target: squad(), options: [skip_commander: true])
 
@@ -57,7 +69,7 @@ defmodule Squadster.Accounts.Tasks.NotifySpec do
               """
                 {
                   "text": "Who is John Galt?",
-                  "target": 1
+                  "target": 2
                 }
               """,
               _headers
@@ -67,7 +79,7 @@ defmodule Squadster.Accounts.Tasks.NotifySpec do
 
         context "when skip_deputy set to true" do
           before do
-            deputy_user = insert(:user, id: 2)
+            deputy_user = insert(:user, id: 3)
             insert(:squad_member, role: :deputy_commander, squad: squad(), user: deputy_user)
           end
 
@@ -80,7 +92,7 @@ defmodule Squadster.Accounts.Tasks.NotifySpec do
               """
                 {
                   "text": "Who is John Galt?",
-                  "target": 2
+                  "target": 3
                 }
               """,
               _headers
@@ -90,7 +102,7 @@ defmodule Squadster.Accounts.Tasks.NotifySpec do
 
         context "when skip_journalist set to true" do
           before do
-            journalist_user = insert(:user, id: 2)
+            journalist_user = insert(:user, id: 4)
             insert(:squad_member, role: :journalist, squad: squad(), user: journalist_user)
           end
 
@@ -103,7 +115,7 @@ defmodule Squadster.Accounts.Tasks.NotifySpec do
               """
                 {
                   "text": "Who is John Galt?",
-                  "target": 2
+                  "target": 4
                 }
               """,
               _headers
@@ -112,6 +124,8 @@ defmodule Squadster.Accounts.Tasks.NotifySpec do
         end
 
         context "when there is a user id in the skip list" do
+          let :user, do: insert(:user, id: 5)
+
           it "does not send message to this user" do
             Notify.notify(message: message(), target: squad(), options: [skip: [user().id]])
 
@@ -121,7 +135,7 @@ defmodule Squadster.Accounts.Tasks.NotifySpec do
               """
                 {
                   "text": "Who is John Galt?",
-                  "target": 1
+                  "target": 5
                 }
               """,
               _headers
