baa6b681bc25af59920ab9cf7ea60ecf678d9b9c
Alexander Teslovskiy <artoriousso@gmail.com>
2020-06-29 19:16:59 +0300

    Refactor user schema specs

--- spec/web/schema/users_spec.exs
+++ spec/web/schema/users_spec.exs
@@ -5,84 +5,21 @@ defmodule Squadster.Web.Schema.UsersSpec do
   alias Squadster.Accounts.User
   alias Squadster.Helpers.Dates
 
-  let :update do
-    """
-      mutation updateUser(
-        $first_name:   String,
-        $last_name:    String,
-        $birth_date:   Date,
-        $university:   String,
-        $faculty:      String,
-        $mobile_phone: String
-      ) {
-        updateUser(
-          firstName:   $first_name,
-          lastName:    $last_name,
-          birthDate:   $birth_date,
-          mobilePhone: $mobile_phone,
-          university:  $university,
-          faculty:     $faculty
-        ) {
-          firstName
-          lastName
-          birthDate
-          mobilePhone
-          university
-          faculty
-        }
-      }
-    """
-  end
-
-  let :list_users do
-    """
-      query getUsers {
-        users {
-          id
-        }
-      }
-    """
-  end
-
-  let :get_user_by_id do
-    """
-      query getUser($id: Int) {
-        user(id: $id) {
-          id
-        }
-      }
-    """
-  end
-
-  let :get_current_user do
-    """
-      query getCurrentUser {
-        currentUser {
-          id
-        }
-      }
-    """
-  end
-
-  let :params, do: %{
-    first_name: Faker.Name.first_name,
-    last_name: Faker.Name.last_name,
-    birth_date: Dates.date_to_string(Faker.Date.date_of_birth),
-    mobile_phone: "+375" <> Integer.to_string(Enum.random(100_000_000..999_999_999)),
-    university: Faker.Industry.sector,
-    faculty: Faker.Industry.sub_sector
-  }
-
-  let :update_user, do: %{query: update(), variables: params()}
-  let :users, do: %{query: list_users()}
-  let :user_query, do: %{query: get_user_by_id(), variables: %{id: user().id}}
-  let :current_user, do: %{query: get_current_user()}
-
   let :user, do: insert(:user)
 
   describe "queries" do
     describe "users" do
       let! :user, do: insert(:user)
+      let :users, do: %{query: users_query()}
+      let :users_query do
+        """
+          query getUsers {
+            users {
+              id
+            }
+          }
+        """
+      end
 
       it "returns a list of users" do
         users_count = entities_count(User)
@@ -97,9 +34,20 @@ defmodule Squadster.Web.Schema.UsersSpec do
     end
 
     describe "user" do
+      let :user_params, do: %{query: user_query(), variables: %{id: user().id}}
+      let :user_query do
+        """
+          query getUser($id: Int) {
+            user(id: $id) {
+              id
+            }
+          }
+        """
+      end
+
       it "returns a user by id" do
         %{"data" => %{"user" => found_user}} = user()
-        |> api_request(user_query())
+        |> api_request(user_params())
         |> json_response(200)
 
         expect found_user["id"]
@@ -109,6 +57,17 @@ defmodule Squadster.Web.Schema.UsersSpec do
     end
 
     describe "current_user" do
+      let :current_user, do: %{query: current_user_query()}
+      let :current_user_query do
+        """
+          query getCurrentUser {
+            currentUser {
+              id
+            }
+          }
+        """
+      end
+
       it "returns a current user" do
         %{"data" => %{"currentUser" => found_user}} = user()
         |> api_request(current_user())
@@ -123,6 +82,47 @@ defmodule Squadster.Web.Schema.UsersSpec do
 
   describe "mutations" do
     describe "update_user" do
+      let :update_user, do: %{query: update_user_mutation(), variables: params()}
+      let :update_user_mutation do
+        """
+          mutation updateUser(
+            $first_name:   String,
+            $last_name:    String,
+            $birth_date:   Date,
+            $university:   String,
+            $faculty:      String,
+            $mobile_phone: String
+          ) {
+            updateUser(
+              firstName:   $first_name,
+              lastName:    $last_name,
+              birthDate:   $birth_date,
+              mobilePhone: $mobile_phone,
+              university:  $university,
+              faculty:     $faculty
+            ) {
+              firstName
+              lastName
+              birthDate
+              mobilePhone
+              university
+              faculty
+            }
+          }
+        """
+      end
+
+      let :params do
+        %{
+          first_name: Faker.Name.first_name,
+          last_name: Faker.Name.last_name,
+          birth_date: Dates.date_to_string(Faker.Date.date_of_birth),
+          mobile_phone: "+375" <> Integer.to_string(Enum.random(100_000_000..999_999_999)),
+          university: Faker.Industry.sector,
+          faculty: Faker.Industry.sub_sector
+        }
+      end
+
       it "updates user's attributes" do
         user() |> api_request(update_user())
 
