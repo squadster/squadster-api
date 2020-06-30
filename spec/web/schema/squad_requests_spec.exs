7550fe297247b07c993d467011403bb3c1928274
Alexander Teslovskiy <artoriousso@gmail.com>
2020-06-30 12:53:36 +0300

    Refactor squad_request schema specs

--- spec/web/schema/squad_requests_spec.exs
+++ spec/web/schema/squad_requests_spec.exs
@@ -7,55 +7,26 @@ defmodule Squadster.Web.Schema.SquadRequestSpec do
   alias Squadster.Formations.SquadRequest
   alias Squadster.Formations.Tasks.NormalizeQueue
 
-  let :create do
-    """
-      mutation createSquadRequest($squad_id: String) {
-        createSquadRequest(squad_id: $squad_id) {
-          id
-          insertedAt
-        }
-      }
-    """
-  end
-
-  let :delete do
-    """
-      mutation deleteSquadRequest($id: Int) {
-        deleteSquadRequest(id: $id) {
-          insertedAt
-        }
-      }
-    """
-  end
-
-  let :approve do
-    """
-      mutation approveSquadRequest($id: Int) {
-        approveSquadRequest(id: $id) {
-          id
-        }
-      }
-    """
-  end
-
-  let :create_squad_request_query, do: %{query: create(), variables: %{squad_id: squad().id}}
-
   let :squad, do: insert(:squad)
   let :user, do: insert(:user)
 
-  def delete_squad_request_query(id) do
-    %{query: delete(), variables: %{id: id}}
-  end
-
-  def approve_squad_request_query(id) do
-    %{query: approve(), variables: %{id: id}}
-  end
-
   describe "mutations" do
     describe "create_squad_request" do
+      let :create_squad_request, do: %{query: create_squad_request_mutation(), variables: %{squad_id: squad().id}}
+      let :create_squad_request_mutation do
+        """
+          mutation createSquadRequest($squad_id: String) {
+            createSquadRequest(squad_id: $squad_id) {
+              id
+              insertedAt
+            }
+          }
+        """
+      end
+
       it "creates a new squad_request with valid attributes" do
         previous_count = entities_count(SquadRequest)
-        api_request(create_squad_request_query())
+        api_request(create_squad_request())
         expect entities_count(SquadRequest) |> to(eq previous_count + 1)
       end
 
@@ -67,7 +38,7 @@ defmodule Squadster.Web.Schema.SquadRequestSpec do
         it "should return error with message" do
           %{"errors" => [%{"message" => message}]} =
             user()
-            |> api_request(create_squad_request_query())
+            |> api_request(create_squad_request())
             |> json_response(200)
 
           expect message |> to_not(be_nil())
@@ -77,10 +48,23 @@ defmodule Squadster.Web.Schema.SquadRequestSpec do
 
     describe "delete_squad_request" do
       let! :squad_request, do: insert(:squad_request, user: user())
+      let :delete_squad_request_mutation do
+        """
+          mutation deleteSquadRequest($id: Int) {
+            deleteSquadRequest(id: $id) {
+              insertedAt
+            }
+          }
+        """
+      end
+
+      def delete_squad_request(id) do
+        %{query: delete_squad_request_mutation(), variables: %{id: id}}
+      end
 
       it "deletes existing squad_request" do
         previous_count = entities_count(SquadRequest)
-        user() |> api_request(delete_squad_request_query(squad_request().id))
+        user() |> api_request(delete_squad_request(squad_request().id))
         expect entities_count(SquadRequest) |> to(eq previous_count - 1)
       end
     end
@@ -88,6 +72,19 @@ defmodule Squadster.Web.Schema.SquadRequestSpec do
     describe "approve_squad_request" do
       let! :squad_request, do: insert(:squad_request, user: insert(:user), squad: squad())
       let :squad, do: build(:squad) |> with_commander(user()) |> insert
+      let :approve_squad_request_mutation do
+        """
+          mutation approveSquadRequest($id: Int) {
+            approveSquadRequest(id: $id) {
+              id
+            }
+          }
+        """
+      end
+
+      def approve_squad_request(id) do
+        %{query: approve_squad_request_mutation(), variables: %{id: id}}
+      end
 
       before do
         mock NormalizeQueue, :start_link
@@ -96,7 +93,7 @@ defmodule Squadster.Web.Schema.SquadRequestSpec do
       it "approves existing squad_request" do
         expect squad_request().approved_at |> to(eq nil)
 
-        user() |> api_request(approve_squad_request_query(squad_request().id))
+        user() |> api_request(approve_squad_request(squad_request().id))
         request = SquadRequest |> Repo.get(squad_request().id)
 
         expect request.approved_at |> to_not(eq nil)
