097ff0833df16ac8e55871b7e9fd8338c5953fdd
Alexander Teslovskiy <artoriousso@gmail.com>
2020-10-21 22:53:40 +0300

    Update minor Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Squadster.MixProject do
     [
       app: :squadster,
       version: "0.1.0",
-      elixir: "~> 1.9.0", # compiled with OTP 20
+      elixir: "~> 1.9.4", # OTP 23
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
