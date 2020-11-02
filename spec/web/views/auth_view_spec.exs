d72e4ced7220705d8a59c81a9cf0bcd33d487ef6
Alexander Teslovskiy <artoriousso@gmail.com>
2020-11-02 22:16:05 +0300

    Fix view spec

--- spec/web/views/auth_view_spec.exs
+++ spec/web/views/auth_view_spec.exs
@@ -5,12 +5,17 @@ defmodule Squadster.Web.AuthViewSpec do
   describe "callback.json" do
     let :user, do: insert(:user)
     let :content do
-      render(SquadsterWeb.AuthView, "callback.json", user: user())
+      render(SquadsterWeb.AuthView, "callback.json", user: user(), show_info: true)
     end
 
-    it "renders json" do
+    it "renders user info" do
       %{user: user} = content()
       expect user |> to(eq(user() |> Map.take(Squadster.Accounts.User.user_fields)))
+    end
+
+    it "renders show_info" do
+      %{show_info: show_info} = content()
+      expect show_info |> to(eq true)
     end
   end
 end
