12fba26d3ec98590651d6fcf458e55f7614d8661
Alexander Teslovskiy <artoriousso@gmail.com>
2020-12-05 22:06:37 +0300

    Remove unnecessary variables from deploy script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ MIX_ENV=prod mix compile
 MIX_ENV=prod mix release
 
 echo $GITHUB_TOKEN | docker login -u $GITHUB_USER --password-stdin docker.pkg.github.com
-docker build -t docker.pkg.github.com/$GITHUB_ORGANIZATION/$GITHUB_REPO/$GITHUB_PACKAGE:$RELEASE_VERSION .
-docker push docker.pkg.github.com/$GITHUB_ORGANIZATION/$GITHUB_REPO/$GITHUB_PACKAGE:$RELEASE_VERSION
+docker build -t docker.pkg.github.com/squadster/squadster-api/release:$RELEASE_VERSION .
+docker push docker.pkg.github.com/squadster/squadster-api/release:$RELEASE_VERSION
